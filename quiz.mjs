eeae512d78f1185ef8e39679de26383a6162d880
Dylan Palmboom <nofxboy1234@gmail.com>
2024-01-02 13:47:36 +0200

    Break out switch statement into individual classes

--- quiz.mjs
+++ quiz.mjs
@@ -1,46 +1,66 @@
+class BooleanQuestion {
+  constructor(description) {
+    this.description = description;
+  }
+
+  printQuestionChoices() {
+    console.log('1. True');
+    console.log('2. False');
+  }
+}
+
+class MultipleChoiceQuestion {
+  constructor(description, options) {
+    this.description = description;
+    this.options = options;
+  }
+
+  printQuestionChoices() {
+    this.options.forEach((option, index) => {
+      console.log(`${index + 1}. ${option}`);
+    });
+  }
+}
+
+class TextQuestion {
+  constructor(description) {
+    this.description = description;
+  }
+
+  printQuestionChoices() {
+    console.log('Answer: _______________');
+  }
+}
+
+class RangeQuestion {
+  constructor(description) {
+    this.description = description;
+  }
+
+  printQuestionChoices() {
+    console.log('Minimum: _______________');
+    console.log('Maximum: _______________');
+  }
+}
+
 function printQuiz(questions) {
   questions.forEach((question) => {
     console.log(question.description);
-    switch (question.type) {
-      case 'boolean':
-        console.log('1. True');
-        console.log('2. False');
-        break;
-      case 'multipleChoice':
-        question.options.forEach((option, index) => {
-          console.log(`${index + 1}. ${option}`);
-        });
-        break;
-      case 'text':
-        console.log('Answer: _______________');
-        break;
-      case 'range':
-        console.log('Minimum: _______________');
-        console.log('Maximum: _______________');
-        break;
-    }
+    question.printQuestionChoices();
     console.log('');
   });
 }
 
 const questions = [
-  {
-    type: 'boolean',
-    description: 'This video is useful.',
-  },
-  {
-    type: 'multipleChoice',
-    description: 'What is your favorite language?',
-    options: ['CSS', 'HTML', 'JS', 'Python'],
-  },
-  {
-    type: 'text',
-    description: 'Describe your favorite JS feature.',
-  },
-  {
-    type: 'range',
-    description: 'What is the speed limit in your city?',
-  },
+  new BooleanQuestion('This video is useful.'),
+  new MultipleChoiceQuestion('What is your favorite language?', [
+    'CSS',
+    'HTML',
+    'JS',
+    'Python',
+  ]),
+  new TextQuestion('Describe your favorite JS feature.'),
+  new RangeQuestion('What is the speed limit in your city?'),
 ];
 
 printQuiz(questions);
