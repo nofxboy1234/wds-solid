9846046b7bba320f1ab0670ec2a84576010b02f8
Dylan Palmboom <nofxboy1234@gmail.com>
2024-01-03 10:42:26 +0200

    Extend from FlyingBird and SwimmingBird instead of Bird class

--- bird.js
+++ bird.js
@@ -1,31 +1,33 @@
-class Bird {
+class FlyingBird {
   fly() {
     console.log('I can fly');
   }
 }
 
-class Duck extends Bird {
-  quack() {
-    console.log('I can quack');
-  }
-}
-
-class Penguin extends Bird {
-  fly() {
-    throw new Error('Cannot fly');
-  }
-
+class SwimmingBird {
   swim() {
     console.log('I can swim');
   }
 }
 
-function makeBirdFly(bird) {
+class Duck extends FlyingBird {
+  quack() {
+    console.log('I can quack');
+  }
+}
+
+class Penguin extends SwimmingBird {}
+
+function makeFlyingBirdFly(bird) {
   bird.fly();
+}
+
+function makeSwimmingBirdSwim(bird) {
+  bird.swim();
 }
 
 const duck = new Duck();
 const penguin = new Penguin();
 
-makeBirdFly(duck);
-makeBirdFly(penguin);
+makeFlyingBirdFly(duck);
+makeSwimmingBirdSwim(penguin);
